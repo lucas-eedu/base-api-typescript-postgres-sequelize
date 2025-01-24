ced852b3bfd415a4f9cc78e5cc1a45cae7850e17
Lucas Eduardo <lucas01.dev@gmail.com>
2025-01-24 17:08:44 -0300

    Setup Typescript/Express + PostgreSQL/Sequelize + Docker

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,7 +1,9 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import helmet from 'helmet';
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import Logger from './config/logger';
 import morganMiddleware from './middlewares/morganMiddleware';
 import router from './routes/main.routes';
@@ -10,10 +12,18 @@ import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Too many requests from this IP, please try again later.',
+});
+
+app.use(helmet());
 app.use(express.json());
 app.use(morganMiddleware);
 app.use('/api', router);
 app.use(errorHandler);
+app.use(limiter);
 
 app.listen(process.env.APP_PORT, async () => {
   try {
